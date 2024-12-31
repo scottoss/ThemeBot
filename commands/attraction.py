376a16e0a66b61496c624e52300b51159d567151
derpyhooves <56843137+scottoss@users.noreply.github.com>
2024-12-31 14:15:07 +0100

    Update attraction.py

--- commands/attraction.py
+++ commands/attraction.py
@@ -25,7 +25,7 @@ async def clear_tracked(interaction):
 
 @decorators.require_destinations
 async def get(interaction, attraction_name, park_name, destination_name):
-    await interaction.response.defer()
+    #await interaction.response.defer()
 
     destination_ids = db.get_user_destination_ids(interaction.user.id)
 
@@ -185,9 +185,9 @@ async def get(interaction, attraction_name, park_name, destination_name):
 
         message_embed.set_image(url="attachment://image.png")
 
-        #return await interaction.followup.send(
-        #    file=img_file, embed=message_embed
-        #)
+        return await interaction.followup.send(
+            file=img_file, embed=message_embed
+        )
 
     # TODO: Add return times if it exists for that attraction
 
